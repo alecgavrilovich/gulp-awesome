79bc0dee3d6e2282d6094af1312e1413aa2a4a46
Alec <alecgavrilovich@gmail.com>
2017-03-30 12:19:20 +0200

    babel

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,28 @@
-const gulp            = require('gulp');
-const browserSync     = require('browser-sync');
-const sass            = require('gulp-sass');
-const autoprefixer    = require('gulp-autoprefixer');
-const sourcemaps      = require('gulp-sourcemaps');
-const useref          = require('gulp-useref');
-const babel           = require('gulp-babel');
-const uglify          = require('gulp-uglify');
-const gulpIf          = require('gulp-if');
-const cssnano         = require('gulp-cssnano');
-const pug             = require('gulp-pug');
-const htmlmin         = require('gulp-htmlmin');
-const htmlbeautify    = require('gulp-html-beautify');
-const imagemin        = require('gulp-imagemin');
-const cache           = require('gulp-cache');
-const del             = require('del');
-const runSequence     = require('run-sequence');
+let config = {
+	jsConcatFiles: [
+		'./app/assets/js/main.js'
+	]
+}
+
+const gulp = require('gulp');
+const browserSync = require('browser-sync');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const sourcemaps = require('gulp-sourcemaps');
+const useref = require('gulp-useref');
+const babel = require('gulp-babel');
+const concat = require('gulp-concat');
+const rename = require('gulp-rename');
+const uglify = require('gulp-uglify');
+const gulpIf = require('gulp-if');
+const cssnano = require('gulp-cssnano');
+const pug = require('gulp-pug');
+const htmlmin = require('gulp-htmlmin');
+const htmlbeautify = require('gulp-html-beautify');
+const imagemin = require('gulp-imagemin');
+const cache = require('gulp-cache');
+const del = require('del');
+const runSequence = require('run-sequence');
 
 
 // Error function - this function prevets browsersync session from crashing on error
@@ -30,42 +38,62 @@ let errorlog = err => {
 
 // Preprocesing pug files to html
 gulp.task('pug', () => {
-     gulp.src(['app/assets/pug/**/*.pug', '!app/assets/pug/includes/*.pug']) 
-    .pipe(pug())
-    .pipe(htmlbeautify()) 
-    .on('error', errorlog) // Passes it through a gulp-pug, log errors to console
-    .pipe(gulp.dest('app/')) // Outputs it in the index.html to app folder
-    .pipe(browserSync.reload({ // Reloading with Browser Sync
-      stream: true
-    }));
+	gulp.src(['app/assets/pug/**/*.pug', '!app/assets/pug/includes/*.pug'])
+		.pipe(pug())
+		.pipe(htmlbeautify())
+		.on('error', errorlog) // Passes it through a gulp-pug, log errors to console
+		.pipe(gulp.dest('app/')) // Outputs it in the index.html to app folder
+		.pipe(browserSync.reload({ // Reloading with Browser Sync
+			stream: true
+		}));
 });
 
 // Preprocesing scss files to css
 gulp.task('sass', () => {
-     gulp.src('app/assets/scss/*.scss') // Gets all files ending with .scss in app/scss and children dirs
-    .pipe(sass().on('error', sass.logError)) // Passes it through a gulp-sass, log errors to console
-    .pipe(gulp.dest('app/assets/css')) // Outputs it in the css folder
-    .pipe(browserSync.reload({ // Reloading with Browser Sync
-      stream: true
-    }));
+	gulp.src('app/assets/scss/*.scss') // Gets all files ending with .scss in app/scss and children dirs
+		.pipe(sass().on('error', sass.logError)) // Passes it through a gulp-sass, log errors to console
+		.pipe(autoprefixer())
+		.pipe(gulp.dest('app/assets/css')) // Outputs it in the css folder
+		.pipe(browserSync.reload({ // Reloading with Browser Sync
+			stream: true
+		}));
 });
+
+// Compile js files with babel and concat
+gulp.task('babel', () => {
+	gulp.src(config.jsConcatFiles) 
+		.pipe(concat('temp.js'))
+		.pipe(babel())
+		.pipe(rename('main.js'))		
+		.pipe(gulp.dest('app/assets/js'))
+});
+
 
 // Start browserSync server
 gulp.task('browserSync', () => {
-  browserSync({
-    server: {
-      baseDir: 'app'
-    },
-    notify: false
-  })
+	browserSync({
+		server: {
+			baseDir: 'app'
+		},
+		notify: false
+	})
 });
+
+// task to run build server for testing final app
+gulp.task('build:serve', function() {
+    browserSync({
+        server: {
+            baseDir: "./dist/"
+        }
+    });
+});
+
 
 // Watchers
 gulp.task('watch', () => {
-  gulp.watch('app/assets/scss/**/*.scss', ['sass']);
-  gulp.watch('app/assets/pug/**/*.pug', ['pug']);
-  //gulp.watch('app/index.html', browserSync.reload);
-  gulp.watch('app/assets/js/**/*.js', browserSync.reload);
+	gulp.watch('app/assets/scss/**/*.scss', ['sass']);
+	gulp.watch('app/assets/pug/**/*.pug', ['pug']);
+	gulp.watch('app/assets/js/**/*.js', browserSync.reload);
 });
 
 
@@ -76,47 +104,46 @@ gulp.task('watch', () => {
 
 // Optimizing CSS and JavaScript 
 gulp.task('useref', () => {
-    gulp.src('app/*.html')
-    .pipe(sourcemaps.init())
-      .pipe(useref())
-      .pipe(gulpIf('*.js', babel({
-              presets: ['es2015']
-          })))
-      .pipe(gulpIf('*.js', uglify()))
-      .pipe(gulpIf('*.css', cssnano()))
-      .pipe(gulpIf('*.html', htmlmin({collapseWhitespace: true})))
-    .pipe(sourcemaps.write('../maps/'))
-    .pipe(gulp.dest('dist'));
+	gulp.src('app/*.html')
+		.pipe(sourcemaps.init())
+			.pipe(useref())
+			.pipe(gulpIf('*.js', uglify()))
+			.pipe(gulpIf('*.css', cssnano()))
+			.pipe(gulpIf('*.html', htmlmin({
+				collapseWhitespace: true
+			})))
+		.pipe(sourcemaps.write('../maps/'))
+		.pipe(gulp.dest('dist'));
 });
 
 
 // Optimizing Images 
 gulp.task('images', () => {
-    gulp.src('app/assets/img/**/*.+(png|jpg|jpeg|gif|svg)')
-    // Caching img that ran through imagemin
-    .pipe(cache(imagemin({
-      interlaced: true,
-    })))
-    .pipe(gulp.dest('dist/assets/img'))
+	gulp.src('app/assets/img/**/*.+(png|jpg|jpeg|gif|svg)')
+		// Caching img that ran through imagemin
+		.pipe(cache(imagemin({
+			interlaced: true,
+		})))
+		.pipe(gulp.dest('dist/assets/img'))
 });
 
 
 // Copying fonts 
 gulp.task('fonts', () => {
-    gulp.src('app/assets/fonts/**/*')
-    .pipe(gulp.dest('dist/assets/fonts'))
+	gulp.src('app/assets/fonts/**/*')
+		.pipe(gulp.dest('dist/assets/fonts'))
 })
 
 
 // Cleaning 
 gulp.task('clean', () => {
-    del.sync('dist').then((cb) => {
-     cache.clearAll(cb);
-  });
+	del.sync('dist').then((cb) => {
+		cache.clearAll(cb);
+	});
 })
 
 gulp.task('clean:dist', () => {
-    del.sync(['dist/**/*', '!dist/img', '!dist/img/**/*']);
+	del.sync(['dist/**/*', '!dist/img', '!dist/img/**/*']);
 });
 
 
@@ -125,17 +152,16 @@ gulp.task('clean:dist', () => {
 // ---------------
 
 gulp.task('default', (callback) => {
-  runSequence(['pug', 'sass', 'browserSync'], 'watch',
-    callback
-  )
+	runSequence(['pug', 'sass', 'browserSync'], 'watch',
+		callback
+	)
 });
 
 gulp.task('build', (callback) => {
-  runSequence(
-    'clean:dist',
-    'pug',
-    'sass',
-    ['useref', 'images', 'fonts'],
-    callback
-  )
+	runSequence(
+		'clean:dist',
+		'pug',
+		'sass', ['useref', 'images', 'fonts'], 'build:serve',
+		callback
+	)
 });
